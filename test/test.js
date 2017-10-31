260458f346dc32f622a4d2b135be1ef889b841fb
Eliran Pe'er <eliran.peer@forter.com>
2017-10-31 21:36:44 +0200

    add passing test

--- test/test.js
+++ test/test.js
@@ -4,4 +4,8 @@ describe('one', () => {
     it('should be equal to two', () => {
         assert(1 === 2);
     });
+
+    it('should be greather than zero', () => {
+        assert(1 > 0);
+    })
 });
